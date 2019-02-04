fb66f752f389d5c4d45b3c46e6848f8e93580e9f
Artsiom Grintsevich <greenfrvr@gmail.com>
2019-02-04 15:30:54 +0300

    no message

--- LanguageSwitcher/ViewController.swift
+++ LanguageSwitcher/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 //все закоментированное это другой способ изменения языка
 //*************
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var englishButton: UIButton!
     @IBOutlet weak var arabicButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let currentLang = Locale.preferredLanguages[0]
         if (currentLang == "ar") {
             arabicButton.isEnabled = false
@@ -24,79 +25,87 @@ class ViewController: UIViewController {
             englishButton.isEnabled = false
         }
     }
-
+    
     @IBAction func showLanguage(_ sender: UIButton) {
-        var currentLang = Locale.current.languageCode
+        let currentLang = Locale.current.languageCode
         let alert = UIAlertController(title: "Language", message: currentLang, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
             alert.dismiss(animated: true, completion: nil)
-        }))
+        })
+        
         self.present(alert, animated: true, completion: nil)
     }
     
     @IBAction func changeToEnglish(_ sender: UIButton) {
-//        let original = class_getClassMethod(NSLocale.self, #selector(getter: NSLocale.current))!
-//        var currentLang = Locale.current.languageCode
-//        print("current lang was", currentLang)
-//        let swizzled = class_getClassMethod(NSLocale.self, #selector(NSLocale.changeLanguageToEn))!
-//        method_exchangeImplementations(original, swizzled)
-//        currentLang = Locale.current.languageCode
-//        print("current lang switched to ", currentLang)
-        language(language:"Base")
+        //        let original = class_getClassMethod(NSLocale.self, #selector(getter: NSLocale.current))!
+        //        var currentLang = Locale.current.languageCode
+        //        print("current lang was", currentLang)
+        //        let swizzled = class_getClassMethod(NSLocale.self, #selector(NSLocale.changeLanguageToEn))!
+        //        method_exchangeImplementations(original, swizzled)
+        //        currentLang = Locale.current.languageCode
+        //        print("current lang switched to ", currentLang)
+        changeLanguage(language:"Base")
         arabicButton.isEnabled = true
         englishButton.isEnabled = false
     }
     
     @IBAction func changeToArabic(_ sender: UIButton) {
-//        var currentLang = Locale.current.languageCode
-//        let original = class_getClassMethod(NSLocale.self, #selector(getter: NSLocale.current))!
-//        print("current lang was", currentLang)
-//        let swizzled = class_getClassMethod(NSLocale.self, #selector(NSLocale.changeLanguageToAr))!
-//        method_exchangeImplementations(original, swizzled)
-//        currentLang = Locale.current.languageCode
-//        print("current lang switched to ", currentLang)
-        language(language:"ar")
+        //        var currentLang = Locale.current.languageCode
+        //        let original = class_getClassMethod(NSLocale.self, #selector(getter: NSLocale.current))!
+        //        print("current lang was", currentLang)
+        //        let swizzled = class_getClassMethod(NSLocale.self, #selector(NSLocale.changeLanguageToAr))!
+        //        method_exchangeImplementations(original, swizzled)
+        //        currentLang = Locale.current.languageCode
+        //        print("current lang switched to ", currentLang)
+        changeLanguage(language:"ar")
         arabicButton.isEnabled = false
         englishButton.isEnabled = true
     }
     
     @IBAction func printString(_ sender: UIButton) {
-        let string = NSLocalizedString("Welcome", comment: "")
-        
-        let alert = UIAlertController(title: "String", message: string, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        let alert = UIAlertController(title: NSLocalizedString("Hello", comment: ""), message: "Welcome".localized, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
             alert.dismiss(animated: true, completion: nil)
-        }))
+        })
+        
         self.present(alert, animated: true, completion: nil)
     }
     
-    private func setNativeLanguage(languages: NSArray){
+    //This sets native language for app, no need to handle bundles manualy (requires app restart).
+    private func setNativeLanguage(_ lang: String){
+        var languages = ["en", "ar"]
+        if lang == "ar" { languages.reverse() }
+        
         UserDefaults.standard.set(languages, forKey: "AppleLanguages")
         UserDefaults.standard.synchronize()
     }
     
-    func language(language : String) {
-        var languageCode = UserDefaults.standard
-        languageCode.set(language, forKey: "language")
-        languageCode.synchronize()
+    //Alternative way, we just save language config and using appropriate bundle instead.
+    func changeLanguage(language: String) {
+        setNativeLanguage(language)
         
-        let language = languageCode.string(forKey: "language")!
-        var path = Bundle.main.path(forResource: language, ofType: "lproj")!
-        if path .isEmpty {
-        path = Bundle.main.path(forResource: language, ofType: "lproj")!
-        }
-        let currentLanguage = Bundle(path: path)
-        print(currentLanguage)
+        UserDefaults.standard.set(language, forKey: "language")
+        UserDefaults.standard.synchronize()
     }
 }
 
-//extension NSLocale {
-//    @objc class func changeLanguageToEn() -> NSLocale {
-//        return NSLocale(localeIdentifier: "en")
-//    }
-//
-//    @objc class func changeLanguageToAr() -> NSLocale {
-//        return NSLocale(localeIdentifier: "ar")
-//    }
-//}
+// MARK: Localizable
+public protocol Localizable {
+    var localized: String { get }
+}
 
+extension String: Localizable {
+    public var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.localized, value: "", comment: "")
+    }
+}
+
+extension Bundle {
+    static var localized: Bundle {
+        let language = UserDefaults.standard.string(forKey: "language") ?? "Base"
+        guard let bundlePath = Bundle.main.path(forResource: language, ofType: "lproj"), !bundlePath.isEmpty else {
+            return Bundle.main
+        }
+        return Bundle(path: bundlePath) ?? Bundle.main
+    }
+}
